9029da6566d9893225981e69d6804386528127c8
Colin Forster <colinjforster@gmail.com>
2020-07-11 02:55:17 +0000

    10 Second Timer

--- functions.py
+++ functions.py
@@ -1,1 +1,35 @@
+import os
+import time
+from time import sleep
+
+
+
+from numerals import numbers
+
+def display_title_bar():
+    # Clears the terminal screen, and displays a title bar.
+    os.system('clear')
+              
+    print("\t**********************************************")
+    print("\t***          It's Timer time!              ***")
+    print("\t**********************************************")
+    
+
+
+
+
+for key in numbers:
+    display_title_bar()
+
+    print("\n\n\a")
+
+   
+    print(numbers[key])
+    print("\nSeconds")
+    
+    # Pause for 1 second between batches.
+    sleep(1)
+
+
+
 
